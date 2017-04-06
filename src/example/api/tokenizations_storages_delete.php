f60140a10bb27f515b5289b1a245f13e88774b9e
acgonzalez <unocongafas@gmail.com>
2017-04-06 12:55:21 +0200

    Ejemplos para consulta y baja de tarjetas tokenizadas

---
+++ src/example/api/tokenizations_storages_delete.php
@@ -0,0 +1,50 @@
+<?php
+# -------------------------------------------------
+#   PARAMS
+# -------------------------------------------------
+
+# -------------------------------------------------
+#   TOKENIZATION
+# -------------------------------------------------
+    $cardindex      = 'cardsdk'; # Mandatory
+
+# -------------------------------------------------
+#   PROFILE
+# -------------------------------------------------
+    $profile        = 'profile';
+
+# -------------------------------------------------
+#   HACK
+# -------------------------------------------------
+    if (isset($params)) {
+        extract($params);
+    }
+
+# -------------------------------------------------
+#   AUTH
+# -------------------------------------------------
+    $auth = new Sipay\Actions\Api\TokenizationsStoragesAuth($profile);
+
+    $extra  = array();
+
+    $idrequest = $auth->call($extra);
+
+
+# -------------------------------------------------
+#   CALL
+# -------------------------------------------------
+    $action = new Sipay\Actions\Api\TokenizationsStoragesDelete($profile);
+
+    $extra = array();
+
+    $response = $action->call($idrequest, $cardindex, $extra);
+
+    if(isset($response['ResultCode']) && $response['ResultCode'] == '0') {
+        echo 'Success!';
+    }
+    else {
+        echo 'Not Success!';
+
+    }
+
+    echo '<div><pre>'.print_r($response, true).'</pre></div>';
